081369c5d359133841a0555a31075afa6cce0b72
matthew-lake <mgtlake@outlook.com>
2015-03-22 18:21:02 +1000

    logging percentage complete

--- src/test/streamModel.java
+++ src/test/streamModel.java
@@ -10,7 +10,7 @@ public class streamModel {
     private String playlistUrl;
     private String playlist;
     public String ffmpegPath = "\\working";
-    private String bandwidth;
+    private int bandwidth;
 
     public void prep(String url) {
         url = url.substring(0, url.indexOf('m')) + "media/media_load_hls_mp4.php" + url.split("php")[1];
@@ -18,18 +18,17 @@ public class streamModel {
         playlistUrl = "http" + doc.split("m3u8")[0].split("http")[doc.split("m3u8")[0].split("http").length - 1] + "m3u8";
         playlist = get(playlistUrl);
         System.out.println(playlist);
-        bandwidth = playlist.split("BANDWIDTH=")[1].split(",")[0];
+        bandwidth = Integer.parseInt(playlist.split("BANDWIDTH=")[1].split(",")[0]);
         System.out.println(bandwidth);
-        String command = "-i " + playlistUrl + " -c copy \"" + "test4" + ".ts\"";
+        String command = "-i " + playlistUrl + " -c copy \"" + "test6" + ".ts\" -y";
         System.out.println(command);
-//        download(command);
+        download(command);
     }
 
     public void download(String command){
-        command = "-i Drive.mkv drive6.ts";
+//        command = "-i Drive.mkv drive6.ts";
         ffmpegPath = "C:\\Users\\mgtlake\\Downloads\\ffmpeg-20150312-git-3bedc99-win64-static\\ffmpeg-20150312-git-3bedc99-win64-static\\bin\\";
         String path = ffmpegPath + "ffmpeg.exe";
-//        path = "C:\\Program Files\\Internet Explorer\\iexplore.exe";
         File file = new File(path);
         if (! file.exists()) {
             System.out.println("The file " + path + " does not exist");
@@ -39,7 +38,14 @@ public class streamModel {
             BufferedReader is = new BufferedReader(new InputStreamReader(p.getErrorStream()));
             String line;
             while ((line = is.readLine()) != null) {
-                System.out.println(line);
+                try {
+                    int size = Integer.parseInt(line.split("size=")[1].split("kB")[0].trim());
+                    double percent = 100 * size / (double) bandwidth;
+                    System.out.println(String.valueOf(percent));
+                }
+                catch (Exception e) {
+                    System.out.println(line);
+                }
             }
         }
         catch (IOException e) {
